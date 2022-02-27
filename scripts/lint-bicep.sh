150d1a20aa5875a6718548605da765678b38c260
Graeme Lockley <graeme.lockley@gmail.com>
2022-02-27 08:42:43 +0200

    feature: make the output a little nicer

--- scripts/lint-bicep.sh
+++ scripts/lint-bicep.sh
@@ -6,15 +6,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for FILE in $( ls -1 $SCRIPT_DIR/../infra/*.bicep )
 do
+    echo $FILE
     OUTPUT=$( az bicep build --file $FILE 2>&1 )
     if [[ "$?" -ne "0" ]]
     then
-        echo Lint Failed: $FILE
+        echo Error: Lint Failed
         echo $OUTPUT
         exit 1
     elif [[ "$OUTPUT" -ne "" ]]
     then
-        echo Lint Failed: $FILE
+        echo Warnings: Lint Failed
         echo $OUTPUT
         exit 1
     fi
