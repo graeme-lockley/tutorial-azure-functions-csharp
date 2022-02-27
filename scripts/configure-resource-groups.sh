a363b65507f4c9354f9cf8b4308aba8454567f5c
Graeme Lockley <graeme.lockley@gmail.com>
2022-02-27 12:52:12 +0200

    refactor: remove references to values that are available in .env

--- scripts/configure-resource-groups.sh
+++ scripts/configure-resource-groups.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 az deployment sub create \
     --name "configure-resource-groups" \
-    --location centralus \
+    --location "$RESOURCE_GROUP_LOCATION" \
     --template-file "${SCRIPT_DIR}/../infra/resource-groups.bicep" \
     --parameters "resourceGroupName=$RESOURCE_GROUP_NAME" \
     --parameters "resourceGroupLocation=$RESOURCE_GROUP_LOCATION"
